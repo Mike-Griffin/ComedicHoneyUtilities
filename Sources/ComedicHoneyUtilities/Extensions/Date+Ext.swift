286ff1e703ae1d42bde9504a2191792cc5bf49e0
Mike <mike_griffin@comcast.net>
2023-01-16 16:19:44 -0800

    Added end of month

--- Sources/ComedicHoneyUtilities/Extensions/Date+Ext.swift
+++ Sources/ComedicHoneyUtilities/Extensions/Date+Ext.swift
@@ -8,7 +8,14 @@
 import Foundation
 
 extension Date {
+    
+    //MARK: Month Helpers
     var startOfMonth: Date {
         Calendar.current.dateInterval(of: .month, for: self)!.start
     }
+    
+    public var endOfMonth: Date {
+        Calendar.current.dateInterval(of: .month, for: self)!.end
+    }
+
 }
